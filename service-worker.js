adf3f261a0c39f0e7a68147ce80235e3a76480b6
flpsno <>
2023-01-08 09:28:36 +0000

    Update from https://github.com/flpsno/vimica-front/commit/2c147ac54d26f7f8c22a53e72d8eedec53775a22

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', event => {
+	self.skipWaiting()
+})
+
+self.addEventListener('fetch', event => {
+	event.respondWith(caches.open('vimica').then(cache => {
+		return cache.match(event.request)
+		.then(cachedResponse => {
+			const fetchedResponse = fetch(event.request)
+			.then(networkResponse => {
+				cache.put(event.request, networkResponse.clone())
+				return networkResponse
+			})
+			return cachedResponse || fetchedResponse
+		})
+	}))
+})
